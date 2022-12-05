4ecd5663b9a4036a11a6b65951fe53ee62090b3a
Nicholas Bishop <nickbishop97@gmail.com>
2022-12-05 13:24:56 -0500

    day4/part2: Add solution

---
+++ day4/part2/main.rs
@@ -0,0 +1,62 @@
+use std::io;
+use std::vec::Vec;
+
+fn main() {
+    let mut input = Vec::new();
+
+    loop {
+	let mut line = String::new();
+	
+	match io::stdin().read_line(&mut line) {
+	    Ok(n) => {
+		if n == 0 {
+		    break;
+		}
+		line = line.trim().to_string();
+		input.push(line);
+	    }
+	    Err(_) => {
+		break;
+	    }
+	}
+    }
+
+    let overlap_count = get_overlap_count(&input);
+
+    println!("{overlap_count} pairs of elves have overlapping sections");
+}
+
+fn get_overlap_count(input: &Vec<String>) -> u32 {
+    let mut overlap_count = 0;
+    for line in input {
+	if is_overlapping(line.split(',').collect()) {
+	    overlap_count += 1;
+	}
+    }
+
+    return overlap_count;
+}
+
+fn is_overlapping(line: Vec<&str>) -> bool {
+    let first_range: Vec<u32> =
+	line.first().unwrap()
+	.split("-")
+	.map(|range| range.parse::<u32>().unwrap())
+	.collect();
+    let second_range: Vec<u32> =
+	line.last().unwrap()
+	.split("-")
+	.map(|range| range.parse::<u32>().unwrap())
+	.collect();
+
+    if (first_range.first()..=first_range.last()).contains(&second_range.first())
+	|| (first_range.first()..=first_range.last()).contains(&second_range.last())
+	|| (second_range.first()..=second_range.last()).contains(&first_range.first())
+	|| (second_range.first()..=second_range.last()).contains(&first_range.last()) {
+	    return true;
+	}
+    
+    return false;
+}
+
+
